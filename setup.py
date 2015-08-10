1e8b726404a3e86e5c61d9d70cd2eea5776f57f7
David Taylor <djtaylor13@gmail.com>
2015-08-10 00:44:53 +0000

    Updated the changelog, added compat file, restructured package layout

--- setup.py
+++ setup.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python
-import sys
 from setuptools import setup, find_packages
-
-# Add the local path
-sys.path.append('src/usr/local/lib/python2.7/dist-packages')
 
 # Get the module version
 from lsbinit import __version__
 
-# Module version / long description
-version = __version__
-long_desc = open('DESCRIPTION.rst').read()
-
 # Run the setup
 setup(
-    name='lsbinit',
-    version=version,
-    description='Linux init scripts handler',
-    package_dir={'': 'src/usr/local/lib/python2.7/dist-packages'},
-    long_description=long_desc,
-    author='David Taylor',
-    author_email='djtaylor13@gmail.com',
-    url='http://github.com/djtaylor/python-lsbinit',
-    license='GPLv3',
-    packages=find_packages(),
-    keywords='feedback terminal shell init service',
-    classifiers=[
+    name             = 'lsbinit',
+    version          = __version__,
+    description      = 'Linux init scripts handler',
+    long_description = open('DESCRIPTION.rst').read(),
+    author           = 'David Taylor',
+    author_email     = 'djtaylor13@gmail.com',
+    url              = 'http://github.com/djtaylor/python-lsbinit',
+    license          = 'GPLv3',
+    packages         = find_packages(),
+    keywords         = 'feedback terminal shell init service',
+    classifiers      = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
